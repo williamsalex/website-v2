6930b94eced4c9d077a09a4ec0c34b261c05b4b2
williamsalex <williams_alex@protonmail.com>
2020-05-28 17:24:45 -0400

    fixed line spacing to be dynamic

--- src/MiniNav.js
+++ src/MiniNav.js
@@ -1,26 +1,8 @@
 import React, { useState } from 'react'
-import { Link, Redirect } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
-
-
-function Nav() {
-    const loc = useLocation().pathname
-    console.log('mininav!')
-  return (
-    <div className = 'movers'>
-        <Link to = '/' className = {`${loc === '/' ? 'curr' : ''}mover`} style = {{textDecoration: 'none'}} > /</Link>
-        <Link to = '/demos' className = {`${loc === '/demos' ? 'curr' : ''}mover`} style = {{textDecoration: 'none'}} >/ demos</Link>
-        <Link to = '/work' className = {`${loc === '/work' ? 'curr' : ''}mover`} style = {{textDecoration: 'none'}} >/ work</Link>
-        <Link to = '/about' className = {`${loc === '/about' ? 'curr' : ''}mover`} style = {{textDecoration: 'none'}} >/ about</Link>
-        <Link to = '/contact' className = {`${loc === '/contact' ? 'curr' : ''}mover`} style = {{textDecoration: 'none'}} >/ contact</Link>
-        <a href = '/resume.pdf' rel="noopener" target="_blank" className = 'mover' style = {{textDecoration: 'none'}} >/ resume</a>
-    </div>
-  )
-}
+import { Redirect } from 'react-router-dom'
 
 export default function MiniNav() {
-
-    // const loc = useLocation().pathname
+    
     let locs = ['/','/demos','/work','/about','/contact']
     const [loc, setLoc] = useState(0)
 
