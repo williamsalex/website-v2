c03f67563cbd3c972249eddd4ee9044b838f04f3
williamsalex <williams_alex@protonmail.com>
2020-06-20 18:00:13 -0400

    added first demo

--- src/Demos.js
+++ src/Demos.js
@@ -4,8 +4,8 @@ export default function Demos() {
   return (
     <div className = 'content'>
       <h1 className = "title">demos</h1>
-      <p className = 'title'>under construction</p>
-      <p className = 'subtitle'>check back later</p>
+      <a className="linked-demo" href="https://sleepy-knuth-1af032.netlify.app/" rel="noopener noreferrer" target="_blank"> <p className = 'bigListItem'>valence life sciences website</p> </a>
+      <p className="listItem">in-progress overhaul & modernization of small venture capital firm's website</p>
     </div>
   )
 }
