c132c9fddefde702c7e9e4e82150453636c19b58
williamsalex <williams_alex@protonmail.com>
2020-06-21 19:46:32 -0400

    changed text

--- src/Demos.js
+++ src/Demos.js
@@ -5,7 +5,7 @@ export default function Demos() {
     <div className = 'content'>
       <h1 className = "title">demos</h1>
       <a className="linked-demo" href="https://sleepy-knuth-1af032.netlify.app/" rel="noopener noreferrer" target="_blank"> <p className = 'bigListItem'>valence life sciences website</p> </a>
-      <p className="listItem">in-progress overhaul & modernization of small venture capital firm's website</p>
+      <p className="listItem">in-progress overhaul & modernization of small biotech venture capital firm's website</p>
     </div>
   )
 }
