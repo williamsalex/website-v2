08439478667a735211222e77063051f0ac7169a4
Alex Williams <williams_alex@protonmail.com>
2020-07-09 15:51:03 -0400

    Update Demos.js

--- src/Demos.js
+++ src/Demos.js
@@ -4,8 +4,10 @@ export default function Demos() {
   return (
     <div className = 'content'>
       <h1 className = "title">demos</h1>
-      <a className="linked-demo" href="https://sleepy-knuth-1af032.netlify.app/" rel="noopener noreferrer" target="_blank"> <p className = 'bigListItem'>valence life sciences website</p> </a>
-      <p className="listItem">in-progress overhaul & modernization of small biotech venture capital firm's website</p>
+      <a className="linked-demo" href="https://valencefund.com" rel="noopener noreferrer" target="_blank"> <p className = 'bigListItem'>valence life sciences</p> </a>
+      <p className="listItem">biotech venture capital firm website</p>
+      <a className="linked-demo" href="https://pensive-colden-1176e6.netlify.app" rel="noopener noreferrer" target="_blank"> <p className = 'bigListItem'>calcimedica</p> </a>
+      <p className="listItem">pharmaceutical company website</p>
     </div>
   )
 }
