735e3c7212fa0b272c9756320c088961cb8a6666
williamsalex <williams_alex@protonmail.com>
2020-09-18 20:21:36 -0500

    Updating to more current info

--- src/About.js
+++ src/About.js
@@ -5,23 +5,20 @@ export default function About() {
       <div className = 'content'>
           <h1 className = "title">about</h1>
           <p className = 'bigListItem'>experience</p>
-          <p className = 'listItem'>currently backend engineer at betteryou</p>
-          <p className = 'listItem'>contract web developer at valence life sciences for four months</p>
-          <p className = 'listItem'>webmaster at valence life sciences for two years</p>
+          <p className = 'listItem'>i'm a backend engineer at betteryou right now!</p>
+          <p className = 'listItem'>web developer & webmaster at valence life sciences for two and a half years</p>
           <p className = 'listItem'>algorithmic researcher in northeastern university's math department for one year</p>
           <br/>
           <p className = 'bigListItem'>education</p>
           <p className = 'listItem'>flatiron school software engineering immersive graduate</p>
-          <p className = 'listItem'>associate's degree from bard college in liberal arts</p>
-          <p className = 'listItem'>one year of computer engineering studies at northeastern university</p>
+          <p className = 'listItem'>associate's degree from bard in liberal arts & a year of computer engineering at northeastern</p>
           <br/>
           <p className = 'bigListItem'>volunteering</p>
           <p className = 'listItem'>data analyst for the boston brigade of code for america for four months</p>
           <br/>
           <p className = 'bigListItem'>other</p>
-          <p className = 'listItem'>long distance runner, 1x marathon, 2x half marathon</p>
+          <p className = 'listItem'>long distance runner & cyclist</p>
           <p className = 'listItem'>avid reader & musicophile</p>
-          <p className = 'listItem'>horrible chess player</p>
       </div>
     )
   }
--- src/Work.js
+++ src/Work.js
@@ -4,6 +4,12 @@ export default function Work() {
     return (
         <div className = 'content'>
             <h1 className = "title">work</h1>
+            <p className = 'bigListItem'>betteryou</p>
+            <p className = 'listItem'>wellness app - 2x minnesota startup awards winner</p>
+            <a href='https://betteryou.ai' rel="noopener noreferrer" target="_blank">
+                <img alt = 'link' className = 'icon' src = '/link2.png'></img>
+            </a>
+            <br/>
             <p className = 'bigListItem'>cruze</p>
             <p className = 'listItem'>bicycle routing app - first place at hack nyu 2020</p>
             <a href='https://devpost.com/software/cruze' rel="noopener noreferrer" target="_blank">
