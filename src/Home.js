ffe2caf289b51d9c528e40ff8d7aa90589e7a9c4
Alex Williams <williams_alex@protonmail.com>
2022-05-25 12:50:30 +0100

    Changed name

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
       <div className = 'content'>
-        <h1 className = "title">alexander williams</h1>
+        <h1 className = "title">alex williams</h1>
         <p className = 'title'>software engineer*</p>
         <p className = 'subtitle'>brooklyn, ny</p>
         <br/>
