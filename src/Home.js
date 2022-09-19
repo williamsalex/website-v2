7e3f87cee896b92a01a39f647373285a97fa9fb6
Alex Williams <williams_alex@protonmail.com>
2022-09-19 10:26:02 -0400

    restyled main page

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,11 @@ export default function Home() {
     return (
       <div className = 'content'>
         <h1 className = "title">alex williams</h1>
-        <p className = 'title'>student*</p>
-        <p className = 'subtitle'>annapolis, md (often brooklyn, ny as well)</p>
+        <p className = 'title'>student¹</p>
+        <p className = 'subtitle'>annapolis, md²</p>
         <br/>
-        <p className = 'tinytitle'>*former backend software engineer</p>
+        <p className = 'tinytitle'>¹former backend software engineer</p>
+        <p className = 'tinytitle'>²often brooklyn, ny as well</p>
       </div>
     )
   }
